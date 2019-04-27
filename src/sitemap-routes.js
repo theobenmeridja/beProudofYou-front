81a8467b97f93fccb76d1db712afd73fede67516
Théo <theo.benmeridja.bba@edhec.com>
2019-04-27 09:55:24 +0000

    xml

---
+++ src/sitemap-routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+
+export default (
+
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/about" />
+        <Route path="/gallery"  />
+        <Route path="/projectGallery" />
+        <Route path="/projectDescription" />
+        <Route path="/postuler" />
+        <Route path="/admin"  />
+        <Route path="/adminProjectGallery" />
+        <Route path="/AdminProjectDescription"  />
+        <Route path="/Contact"  />
+        <Route path="/mentionslegales"  />
+        <Route path="/CGU" />
+      </Switch>
+
+);
